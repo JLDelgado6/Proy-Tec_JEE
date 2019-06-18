2ce806876185b326ffe1fa6dcd1dcb21b2a32eed
Manuel <manuelsperod@gmail.com>
2019-06-18 19:23:28 +0200

    Creación clase GET (Sin terminar)

--- Proy-Tec_JEE/src/logic/Get.java
+++ Proy-Tec_JEE/src/logic/Get.java
@@ -1,5 +1,16 @@
 package logic;
 
-public class Get {
+import org.hibernate.Session;
 
+import dbm.HibernateUtil;
+import model.Vehiculo;
+
+public class Get {
+	Long ind = 1L;
+
+	Session session = HibernateUtil.getSession();
+	Vehiculo v = session.get(Vehiculo.class, ind);
+	session.close();
+
+	System.out.println("Vehículo recuperado con éxito: " + v);
 }
