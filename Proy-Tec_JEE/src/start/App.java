1afa316a84f478934164b023caae73c8be9d3178
Manuel <manuelsperod@gmail.com>
2019-06-19 14:14:38 +0200

    Cambios en App

--- Proy-Tec_JEE/src/start/App.java
+++ Proy-Tec_JEE/src/start/App.java
@@ -5,39 +5,15 @@ import java.util.List;
 import org.hibernate.Session;
 
 import dbm.HibernateUtil;
-import model.Persona;
+import model.Vehiculo;
 
 public class App {
 
 	public static void main(String[] args) {
 
-		//addPersona();
-		//getPersona();
-		//edtPersona();
-		//delPersona();
-		//lstPersona();
-		//schPersona();
-
-	}
-
-	@SuppressWarnings("unchecked")
-	private static void lstPersona() {
-		Session session = HibernateUtil.getSession();
-		String query = "FROM Persona";
-		List<Persona> listado = session.createQuery(query).list();
-		session.close();
-
-		for(Persona p : listado) {
-			System.out.println(p);	
-		}
-	}
-
-	private static void schPersona() {
-		Session session = HibernateUtil.getSession();
-		String query = "FROM Persona where salario=100";
-		Persona p = (Persona) session.createQuery(query).uniqueResult();
-		session.close();
-
-		System.out.println("Persona encontrada con éxito: " + p);
+		
+		logic.Post.addVehiculo();
+		logic.GestListaVehiculo.lstVehiculo();
+		
 	}
 }
