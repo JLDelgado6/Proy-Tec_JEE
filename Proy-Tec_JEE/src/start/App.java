174eb1c7546553e4fff93ef2f8d9173c917c4f8a
JLDelgado6 <usuario@AMB07>
2019-06-19 13:55:41 +0200

    App.java corregido (pendiente de recolocar lst- y schPersona)

--- Proy-Tec_JEE/src/start/App.java
+++ Proy-Tec_JEE/src/start/App.java
@@ -20,65 +20,6 @@ public class App {
 
 	}
 
-	private static void addPersona() {
-
-		Persona p = new Persona();
-		p.setNombre("luis");
-		p.setApellidos("Gonzalez");
-		p.setSalario(7600);
-
-		Session session = HibernateUtil.getSession();
-
-		session.beginTransaction();
-		session.save(p);
-		session.getTransaction().commit();
-
-		session.close();
-
-		System.out.println("Persona insertada con éxito: " + p);
-	}
-
-	private static void getPersona() {
-		Long indice = 1L;
-
-		Session session = HibernateUtil.getSession();
-		Persona p = session.get(Persona.class, indice);
-		session.close();
-
-		System.out.println("Persona recuperada con éxito: " + p);
-	}
-
-	private static void edtPersona() {
-		Long indice = 1L;
-
-		Session session = HibernateUtil.getSession();
-		Persona p = session.get(Persona.class, indice);
-		p.setSalario(100);
-
-		session.beginTransaction();
-		session.update(p);
-		session.getTransaction().commit();
-		
-		session.close();
-
-		System.out.println("Persona editada con éxito: " + p);
-	}
-
-	private static void delPersona() {
-		Long indice = 1L;
-		
-		Session session = HibernateUtil.getSession();
-		Persona p = session.get(Persona.class, indice);
-
-		session.beginTransaction();
-		session.delete(p);
-		session.getTransaction().commit();
-		
-		session.close();
-
-		System.out.println("Persona eliminada con éxito: " + p);
-	}
-
 	@SuppressWarnings("unchecked")
 	private static void lstPersona() {
 		Session session = HibernateUtil.getSession();
