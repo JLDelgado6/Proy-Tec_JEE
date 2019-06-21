f1be62e581be01c599954c084f4b7b7002d40cb2
Manuel <manuelsperod@gmail.com>
2019-06-21 12:08:41 +0200

    Se ha quitado el try - catch de la clase

--- Proy-Tec_JEE/src/logic/Get.java
+++ Proy-Tec_JEE/src/logic/Get.java
@@ -1,23 +1,21 @@
 package logic;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.hibernate.Session;
 
-import controller.HttpServletRequest;
+
 import dbm.HibernateUtil;
 import model.Vehiculo;
 
 public class Get {
 	public static void getVehiculo(HttpServletRequest request) {
 		Long ind = 1L;
-
 		Session session = HibernateUtil.getSession();
 		Vehiculo v = session.get(Vehiculo.class, ind);
-		try {
 		session.close();
-
 		System.out.println("Vehículo recuperado con éxito: " + v);
-		} catch (Exception e) {
-			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-		}
+
+		
 	}
 }
