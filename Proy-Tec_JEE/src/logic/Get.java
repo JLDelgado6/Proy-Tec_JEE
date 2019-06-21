eabbf99759cff061f336d6027e80636a8bbd45f6
Manuel <manuelsperod@gmail.com>
2019-06-21 12:39:22 +0200

    Modificación en Get para solucionar error

--- Proy-Tec_JEE/src/logic/Get.java
+++ Proy-Tec_JEE/src/logic/Get.java
@@ -9,13 +9,15 @@ import dbm.HibernateUtil;
 import model.Vehiculo;
 
 public class Get {
-	public static void getVehiculo(HttpServletRequest request) {
+	public static String getVehiculo(HttpServletRequest request) {
 		Long ind = 1L;
+		String resultado="";
 		Session session = HibernateUtil.getSession();
 		Vehiculo v = session.get(Vehiculo.class, ind);
 		session.close();
 		System.out.println("Vehículo recuperado con éxito: " + v);
-
+		resultado = v.toString();
+		return resultado;
 		
 	}
 }
