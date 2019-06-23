31664a02cb2690e64c12ffcc6e5251bb443f7501
Manuel <manuelsperod@gmail.com>
2019-06-23 20:01:27 +0200

    Pequeña modificación

--- Proy-Tec_JEE/src/logic/Get.java
+++ Proy-Tec_JEE/src/logic/Get.java
@@ -9,19 +9,25 @@ import dbm.HibernateUtil;
 import model.Vehiculo;
 
 public class Get {
+	
 	public static String getVehiculo(HttpServletRequest request) {
-		Long ind = 1L;
-		String resultado="";
 		
 		System.out.println("Procesando la petición GET");
+	
+		Long index = Long.parseLong(request.getParameter("idVehiculo"));
+		String resultado="";
 		
-		Session session = HibernateUtil.getSession();
-		Vehiculo v = session.get(Vehiculo.class, ind);
-		session.close();
+		Session vSession = HibernateUtil.getSession();
+		Vehiculo v = vSession.get(Vehiculo.class, index);
+		vSession.close();
 		
-		System.out.println("Vehículo recuperado con éxito: " + v);
+		System.out.println("Mostrar vehículo: " + v);
 		resultado = v.toString();
 		return resultado;
 		
 	}
 }
+
+
+
+
